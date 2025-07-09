573f9afa01650b52a18a80719d6a1d005e9fb543
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-07-09 12:31:47 -0500

    chore(php): resolved php errors

--- dynamic-content-plugin.php
+++ dynamic-content-plugin.php
@@ -11,7 +11,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       dynamic-content-plugin
  *
- * @package Utk
+ * @package DynamicContent
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
--- src/cookie-setter/render.php
+++ src/cookie-setter/render.php
@@ -1,9 +1,17 @@
 <?php
 /**
  * Render callback for the Cookie Setter block.
+ *
+ * @package DynamicContent
  */
 
 if ( ! function_exists( 'render_set_cookie_block' ) ) {
+	/**
+	 * Render the JavaScript code to set a cookie.
+	 *
+	 * @param array $attributes Block attributes.
+	 * @return string The script tag for setting a cookie.
+	 */
 	function render_set_cookie_block( $attributes ) {
 		if ( empty( $attributes['cookieKey'] ) || empty( $attributes['cookieValue'] ) ) {
 			return '';
@@ -13,11 +21,16 @@ if ( ! function_exists( 'render_set_cookie_block' ) ) {
 		$value = esc_js( $attributes['cookieValue'] );
 
 		// Encode the value using encodeURIComponent and set cookie in JS.
-		return "<script>
-			document.cookie = '{$key}=' + encodeURIComponent('{$value}') + '; path=/';
-		</script>";
+		ob_start();
+		?>
+		<script>
+			document.cookie = '<?php echo esc_js( $key ); ?>=' + encodeURIComponent('<?php echo esc_js( $value ); ?>') + '; path=/';
+		</script>
+		<?php
+		return ob_get_clean();
 	}
 }
 
 // Output the rendered HTML.
+// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- safe inline script output
 echo render_set_cookie_block( $attributes );
